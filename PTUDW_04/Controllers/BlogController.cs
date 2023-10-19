9db295db24a86ae7075039716e542d56e0c02960
noname44k1 <thanh.cntt.dhv@gmail.com>
2023-10-19 14:08:11 +0700

    19/10/2023 -blog

--- PTUDW_04/Controllers/BlogController.cs
+++ PTUDW_04/Controllers/BlogController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PTUDW_04.Models;
 
 namespace PTUDW_04.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly HarmicContext _context;
+
+        public BlogController(HarmicContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var blogs = _context.TbBlogs.Where(i => (bool)i.IsActive).OrderByDescending(i => i.BlogId).ToList();
+            if (blogs == null)
+            {
+                return NotFound();
+            }
+            ViewBag.blogComment = _context.TbBlogComments.Where(m => (bool)m.IsActive).ToList();
+            return View(blogs);
+        }
+
+        [Route("/blog/{alias}-{id}.html")]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var blog = _context.TbBlogs.Where(i => i.BlogId == id && (bool)i.IsActive).FirstOrDefault();
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            return View(blog);
         }
     }
 }
